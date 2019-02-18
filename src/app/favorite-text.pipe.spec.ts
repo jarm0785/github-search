be798cf303c2b5634161667cea32711ba7e3cddb
jarm0785 <jarm0785@gmail.com>
2019-02-18 20:48:09 +0200

    Updated .gitignore file. Removed unnecesary files. Updated README

--- src/app/favorite-text.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FavoriteTextPipe } from './favorite-text.pipe';
-
-describe('FavoriteTextPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FavoriteTextPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
